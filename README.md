e45039c5a2331d5479f11d6bc9f5116425c99326
forresto <forrest@sembiki.com>
2015-10-23 18:52:41 -0400

    dev setup

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ed
+
+Early prototype to use [ProseMirror](http://prosemirror.net/) with data from [the Grid API](http://developer.thegrid.io/)
+
+The demo shows translating from ProseMirror to the the Grid API JSON and back.
+
+## dev
+
+`npm start` and open [http://localhost:8080/demo/](http://localhost:8080/demo/)
+
+In development mode, webpack builds and serves the targets in memory from /webpack-memory
+
+Changes will trigger a browser refresh.
+
+## test
+
+`npm test`
+
+## build
+
+`npm build`
+
+Outputs minified ed.js to /build
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+webpack = require('webpack');
+var __DEV = (process.env.DEV === 'true');
+
+var entry = {};
+var plugins = [];
+
+if (__DEV) {
+  entry.demo = './demo/demo.js';
+} else {
+  entry.ed = './index.js'
+  plugins.push( new webpack.optimize.UglifyJsPlugin() );
+}
+
+module.exports = {
+  entry: entry,
+  plugins: plugins,
+  output: {
+    path: './build/',
+    publicPath: '/webpack-memory/',
+    filename: '[name].js',
+    library: 'TheGridEd',
+    libraryTarget: 'var'
+  },
+  debug: __DEV,
+  devtool: (__DEV ? 'cheap-module-eval-source-map' : null),
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /-dev-server/ },
+      { test: /\.json$/, loader: 'json-loader' },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.babel.js', '.js', '.json']
+  }
+};
