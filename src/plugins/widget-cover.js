5f5ed337900e43d1a7960ae8ba1c46765607e0de
Forrest Oliphant <forrest@sembiki.com>
2016-01-15 15:27:53 -0500

    minimal cover image view
    Just to get images back in.
    
    Will be a small React app for:
    * attribution editing
    * media viewing

--- src/plugins/widget-cover.js
+++ src/plugins/widget-cover.js
@@ -4,5 +4,16 @@ export default class WidgetCover extends WidgetBase {
   static type () { return 'cover' }
   constructor (options) {
     super(options)
+    
+    let {cover} = this.initialBlock
+    if (cover && cover.src) {
+      this.cover = document.createElement('div')
+      this.cover.style.background = `url(${cover.src})`
+      this.cover.style.backgroundRepeat = 'no-repeat'
+      this.cover.style.backgroundSize = 'contain'
+      this.cover.style.width = '50%'
+      this.cover.style.height = '100%'
+      this.el.appendChild(this.cover)
+    }
   }
 }
