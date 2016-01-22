408b3bfd6f0fb03ad849cd2e5d7370b37da1f5d8
Forrest Oliphant <forrest@sembiki.com>
2016-01-22 16:27:30 +0100

    relative path for iframe

--- src/plugins/widget-code.js
+++ src/plugins/widget-code.js
@@ -2,7 +2,7 @@ import WidgetIframe from './widget-iframe'
 
 export default class WidgetCode extends WidgetIframe {
   static type () { return 'code' }
-  src () { return '../../node_modules/@the-grid/ced/editor/index.html' }
+  src () { return './node_modules/@the-grid/ced/editor/index.html' }
   constructor (options) {
     super(options)
   }
