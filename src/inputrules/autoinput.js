d4470980a5b37f7ac3cc4a45e5dc313f33617678
Forrest Oliphant <forrest@sembiki.com>
2016-02-04 16:58:06 -0500

    WIP ``` rule #25

--- src/inputrules/autoinput.js
+++ src/inputrules/autoinput.js
@@ -1,30 +1,22 @@
 import {InputRule} from 'prosemirror/src/inputrules'
+import {Pos} from 'prosemirror/src/model'
 import {Media} from '../schema/media'
 
 import uuid from 'uuid'
 
+
 Media.register('autoInput', 'replaceWithCodeWidget',
   new InputRule(/^```$/, '`', function (pm, _, pos) {
-    pm.tr.replaceWith(pos, pos, pm.schema.node(this, {
+    setAs(pm, pos, this, {
       id: uuid.v4(),
       type: 'code'
-    })).apply()
+    })
   })
 )
 
-// import {Pos} from 'prosemirror/src/model'
-
-// Media.register('autoInput', 'replaceWithCodeWidget',
-//   new InputRule(/^```$/, '`', function (pm, _, pos) {
-//     setAs(pm, pos, this, {
-//       id: uuid.v4(),
-//       type: 'code'
-//     })
-//   })
-// )
-
-// function setAs (pm, pos, type, attrs) {
-//   pm.tr.setBlockType(pos, pos, type, attrs)
-//        .delete(new Pos(pos.path, 0), pos)
-//        .apply()
-// }
+function setAs (pm, pos, type, attrs) {
+  pm.tr
+    .delete(new Pos(pos.path, 0), pos)
+    .setBlockType(pos, pos, type, attrs)
+    .apply()
+}
