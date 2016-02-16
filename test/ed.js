c8631c15902c1729c86a4309ca3aabde9b6545a7
Forrest Oliphant <forrest@sembiki.com>
2016-02-16 17:22:51 -0500

    merge test #50 fails

--- test/ed.js
+++ test/ed.js
@@ -27,11 +27,59 @@ describe('Ed', function () {
 
   it('on mount it has expected editable html structure', function () {
     const children = ed.pm.content.children
+    expect(children.length).to.equal(3)
     expect(children[0].textContent).to.equal('Title')
     expect(children[0].nodeName).to.equal('H1')
     expect(children[1].textContent).to.equal('Text 1')
     expect(children[1].nodeName).to.equal('P')
     expect(children[2].textContent).to.equal('Text 2')
     expect(children[2].nodeName).to.equal('P')
+  })
+
+  it('it has expected pm document', function () {
+    const content = ed.pm.doc.content.content
+    expect(content.length).to.equal(3)
+    expect(content[0].textContent).to.equal('Title')
+    expect(content[0].type.name).to.equal('heading')
+    expect(content[1].textContent).to.equal('Text 1')
+    expect(content[1].type.name).to.equal('paragraph')
+    expect(content[2].textContent).to.equal('Text 2')
+    expect(content[2].type.name).to.equal('paragraph')
+  })
+  
+  it('replace text with placeholder block', function () {
+    ed.replaceBlock(1, {
+      id: '0000',
+      type: 'placeholder'
+    })
+    const content = ed.pm.doc.content.content
+    expect(content.length).to.equal(3)
+    expect(content[0].textContent).to.equal('Title')
+    expect(content[0].type.name).to.equal('heading')
+    expect(content[1].textContent).to.equal('')
+    expect(content[1].type.name).to.equal('media')
+    expect(content[1].attrs.type).to.equal('placeholder')
+    expect(content[2].textContent).to.equal('Text 2')
+    expect(content[2].type.name).to.equal('paragraph')
+  })
+
+  it('replace placeholder with image block, should correctly merge', function () {
+    ed.replaceBlock(1, {
+      id: '0000',
+      type: 'placeholder'
+    })
+    ed.setContent([{
+      id: '0000',
+      type: 'image'
+    }])
+    const content = ed.pm.doc.content.content
+    expect(content.length).to.equal(3)
+    expect(content[0].textContent).to.equal('Title')
+    expect(content[0].type.name).to.equal('heading')
+    expect(content[1].textContent).to.equal('')
+    expect(content[1].type.name).to.equal('media')
+    expect(content[1].attrs.type).to.equal('image')
+    expect(content[2].textContent).to.equal('Text 2')
+    expect(content[2].type.name).to.equal('paragraph')
   })
 })
