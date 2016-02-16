350ac119677afc30d3bde55cd0220f699560d9e4
Forrest Oliphant <forrest@sembiki.com>
2016-02-16 12:43:36 -0500

    lint

--- test/ed.js
+++ test/ed.js
@@ -15,14 +15,14 @@ describe('Ed', function () {
 
   it('on mount it has expected editable html structure', function (done) {
     const fixture = [
-      {type: 'h1', html: `<h1>Title</h1>`},
-      {type: 'text', html: `<p>Text 1</p>`},
-      {type: 'text', html: `<p>Text 2</p>`}
+      {type: 'h1', html: '<h1>Title</h1>'},
+      {type: 'text', html: '<p>Text 1</p>'},
+      {type: 'text', html: '<p>Text 2</p>'}
     ]
     const expected =
-      `<h1 pm-container="true" pm-offset="0"><span pm-offset="0" pm-leaf="5">Title</span></h1>` +
-      `<p pm-container="true" pm-offset="1"><span pm-offset="0" pm-leaf="6">Text 1</span></p>` +
-      `<p pm-container="true" pm-offset="2"><span pm-offset="0" pm-leaf="6">Text 2</span></p>`
+      '<h1 pm-container="true" pm-offset="0"><span pm-offset="0" pm-leaf="5">Title</span></h1>' +
+      '<p pm-container="true" pm-offset="1"><span pm-offset="0" pm-leaf="6">Text 1</span></p>' +
+      '<p pm-container="true" pm-offset="2"><span pm-offset="0" pm-leaf="6">Text 2</span></p>'
 
     const ed = new Ed({
       container: mount,
