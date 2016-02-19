13972ad5e33b0a3b6f1ded516073e790d7ebaeb0
Forrest Oliphant <forrest@sembiki.com>
2016-02-19 16:02:56 -0500

    placeholder safety

--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -6,6 +6,9 @@ import LinearProgress from 'material-ui/lib/linear-progress'
 
 export default function Placeholder (props) {
   const {metadata} = props.initialBlock
+  if (!metadata) {
+    return el('div', {className: 'Placeholder'})
+  }
   const status = metadata.status || ''
   const value = metadata.progress
   const mode = metadata.progress != null ? 'determinate' : 'indeterminate'
