a3cb1e4c5f683a0abaebf0fa991583d4de04185f
Forrest Oliphant <forrest@sembiki.com>
2016-03-15 22:14:14 -0400

    Media component with context

--- src/components/media.js
+++ src/components/media.js
@@ -1,8 +1,9 @@
-import {createElement as el} from 'react'
+import React, {createElement as el} from 'react'
 
 import Placeholder from './placeholder'
 import AttributionEditor from './attribution-editor'
 import Title from './title'
+import rebassTheme from './rebass-theme'
 
 const Components =
   { placeholder: Placeholder
@@ -10,8 +11,28 @@ const Components =
   , attribution: AttributionEditor
   }
 
-export default function Media (props) {
-  const {type} = props.initialBlock
-  let Component = Components[type] || Components.attribution
-  return el(Component, props)
+class Media extends React.Component {
+  getChildContext () {
+    return (
+      { imgfloConfig: (this.context.imgfloConfig || this.props.imgfloConfig)
+      , store: (this.context.store || this.props.store)
+      , rebass: rebassTheme
+      }
+    )
+  }
+  render () {
+    const {type} = this.props.initialBlock
+    let Component = Components[type] || Components.attribution
+    return el(Component, this.props)
+  }
 }
+Media.contextTypes =
+  { imgfloConfig: React.PropTypes.object
+  , store: React.PropTypes.object
+  }
+Media.childContextTypes =
+  { imgfloConfig: React.PropTypes.object
+  , rebass: React.PropTypes.object
+  , store: React.PropTypes.object
+  }
+export default React.createFactory(Media)
