6d0933cc62b11b9641eaf96c15afbdb3c0306e6d
Forrest Oliphant <forrest@sembiki.com>
2016-03-25 17:53:29 -0400

    case sensitive import
    :eyeglasses:

--- src/util/lodash.js
+++ src/util/lodash.js
@@ -1,7 +1,7 @@
 // Build in minimal version, only _.functions we use
 
 import debounce from 'lodash/debounce'
-import cloneDeep from 'lodash/clonedeep'
+import cloneDeep from 'lodash/cloneDeep'
 const _ = {debounce, cloneDeep}
 
 export default _
