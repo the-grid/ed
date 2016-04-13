c1624968d0b296d3493a45728cceae459b67f063
Forrest Oliphant <forrest@sembiki.com>
2016-04-13 18:18:52 -0400

    lint

--- src/plugins/share-url.js
+++ src/plugins/share-url.js
@@ -10,16 +10,16 @@ function testPrevUrl () {
   // Current position (under potential url line)
   const currentNode = this.ed.pm.doc.childBefore(selection.anchor)
   if (!currentNode || currentNode.index < 1) return
-  
+
   // Potential url line
   const index = currentNode.index - 1
   const prevNode = this.ed.pm.doc.child(index)
   if (!prevNode || prevNode.type.name !== 'paragraph') return
-  
+
   // Test if url
   const url = prevNode.textContent.trim()
   if (!url || !isUrl(url)) return
-  
+
   // Make share
   const id = uuid.v4()
   const block =
