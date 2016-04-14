d30e153fe5c2d1fc5b9a4adef9a16e3e636d8122
Forrest Oliphant <forrest@sembiki.com>
2016-04-14 01:26:36 -0400

    lint

--- src/commands/ed-commands.js
+++ src/commands/ed-commands.js
@@ -6,25 +6,26 @@ const ed_upload_image =
   , menu:
     { group: 'ed_block'
     , display:
-      { render: function (command, pm) {
-        const el = elt('div'
-        , { style:
-            { cursor: 'pointer' }
+      { render:
+          function (command, pm) {
+            const el = elt('div'
+            , { style:
+                { cursor: 'pointer' }
+              }
+            , 'Upload Image'
+            )
+            el.addEventListener('mousedown', function (event) {
+              // HACK around #44
+              event.stopPropagation()
+            })
+            el.addEventListener('click', function (event) {
+              event.stopPropagation()
+              const {index} = pm.doc.childBefore(pm.selection.anchor)
+              if (index == null) return false
+              pm.signal('ed.menu.file', index)
+            })
+            return el
           }
-        , 'Upload Image'
-        )
-        el.addEventListener('mousedown', function (event) {
-          // HACK around #44
-          event.stopPropagation()
-        })
-        el.addEventListener('click', function (event) {
-          event.stopPropagation()
-          const {index} = pm.doc.childBefore(pm.selection.anchor)
-          if (index == null) return false
-          pm.signal('ed.menu.file', index)
-        })
-        return el
-      }
       }
     }
   }
