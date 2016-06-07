56345f0ca770c0cbc2bdfd94b011e92410a45b11
Forrest Oliphant <forrest@sembiki.com>
2016-06-07 16:06:27 -0400

    wip iframe overlay #228

--- src/plugins/widget-iframe.js
+++ src/plugins/widget-iframe.js
@@ -11,12 +11,24 @@ function postInitialBlock () {
   }
 }
 
+function onDragStart (event) {
+  console.log('ds', event.dataTransfer.files.length)
+  this.dropEl.style.display = 'block'
+}
+
+function onDragEnd (event) {
+  this.dropEl.style.display = 'none'
+}
+
+
 export default class WidgetIframe extends WidgetBase {
   static type () { return 'iframe -- extend me' }
   src () { return 'about:blank' }
   constructor (options) {
     super(options)
     this.postInitialBlock = postInitialBlock.bind(this)
+    this.onDragStart = onDragStart.bind(this)
+    this.onDragEnd = onDragEnd.bind(this)
 
     this.initialFocus = options.initialFocus
 
@@ -31,11 +43,31 @@ export default class WidgetIframe extends WidgetBase {
     this.el.appendChild(this.frame)
 
     this.height = 250
+
+    this.el.style.position = 'relative'
+
+    this.dropEl = document.createElement('div')
+    this.dropEl.style.position = 'absolute'
+    this.dropEl.style.top = 0
+    this.dropEl.style.right = 0
+    this.dropEl.style.bottom = 0
+    this.dropEl.style.left = 0
+    this.dropEl.style.display = 'none'
+    this.dropEl.style.backgroundColor = 'red'
+    this.el.appendChild(this.dropEl)
+
+    window.addEventListener('dragover', this.onDragStart)
+    window.addEventListener('dragexit', this.onDragEnd)
   }
   teardown () {
     if (this.initialBlock) {
       this.frame.removeEventListener('load', this.postInitialBlock)
     }
+    // window.removeEventListener('dragstart', this.onDragStart)
+    // window.removeEventListener('dragover', this.onDragStart)
+    // window.removeEventListener('dragleave', this.onDragEnd)
+    // window.removeEventListener('dragexit', this.onDragEnd)
+    // window.removeEventListener('dragend', this.onDragEnd)
     super.teardown()
   }
   getHeight () {
