66dd7e80b57f33f70c8ce967a28509edf78b273f
Forrest Oliphant <forrest@sembiki.com>
2016-06-16 15:48:40 -0400

    Media matchDOMTag / toDOM #242

--- src/schema/media.js
+++ src/schema/media.js
@@ -1,10 +1,11 @@
 require('./media.css')
 
 import {Block, Attribute} from 'prosemirror/src/model'
-// import {NodeKindTop} from './ed-nodes'
+import {isMediaType} from '../convert/types'
 
 export class Media extends Block {
   get isBlock () { return true }
+  get isInline () { return false }
   get isLeaf () { return true }
   get locked () { return true }
   get draggable () { return true }
@@ -17,34 +18,25 @@ export class Media extends Block {
       }
     )
   }
-}
-Media.register('parseDOM', 'div'
-, { tag: 'div'
-  , rank: 9999
-  , parse: function (dom, state) {
-    const id = dom.getAttribute('grid-id')
-    const type = dom.getAttribute('grid-type')
-    if (!id || !type) {
+  get matchDOMTag () {
+    return {'div[grid-type]': function (dom) {
+      const id = dom.getAttribute('grid-id')
+      const type = dom.getAttribute('grid-type')
+      if (id && type && isMediaType(type)) {
+        return {id, type}
+      }
       return false
-    }
-    state.insert(this, {id, type})
+    }}
   }
+  toDOM (node) {
+    const {id, type, height, initialFocus} = node.attrs
+    return ['div'
+    , { 'class': 'EdSchemaMedia'
+      , 'grid-id': id
+      , 'grid-type': type
+      , 'grid-initial-focus': initialFocus
+      , 'style': `height: ${height}px;`
+      }
+    ]
   }
-)
-Media.prototype.serializeDOM = (node, s) => {
-  const {id, type, height, initialFocus} = node.attrs
-  if (!id) {
-    throw new Error('Can not serialize Media div without id')
-  }
-  if (!type) {
-    throw new Error('Can not serialize Media div without type')
-  }
-  return s.elt('div'
-  , { class: 'EdSchemaMedia'
-    , 'grid-id': id
-    , 'grid-type': type
-    , 'grid-initial-focus': initialFocus
-    , style: `height: ${height};`
-    }
-  )
 }
